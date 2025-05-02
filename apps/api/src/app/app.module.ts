ac4e385e42cb63f431d2bdb63386edee07e7a907
daliborstakic <stakicdalibor64@gmail.com>
2025-05-02 11:26:33 +0200

    chore(enviroment): add env file to remove secrets and update dependencies

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -5,19 +5,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@selune-backend/entities';
 import { UserModule } from '@selune-backend/user';
 import { CqrsModule } from '@nestjs/cqrs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/api/.env',
+    }),
     CqrsModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'selune',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT', '5432')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
     }),
     UserModule,
   ],
