f78d00d0f3dba574ee9439647c87f0c84b67bce4
Dalibor Stakić <stakicdalibor64@gmail.com>
2025-05-02 11:35:09 +0200

    chore(project): setup typeorm, cqrs and swagger (#1)
    * chore(project): setup typeorm, cqrs and swagger
    
    * chore(enviroment): add env file to remove secrets and update dependencies
    
    * chore(project): fix pnpm workspace missing packages

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@selune-backend/entities';
+import { UserModule } from '@selune-backend/user';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/api/.env',
+    }),
+    CqrsModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT', '5432')),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
