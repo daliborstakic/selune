3d3ca55047cc2a72370b57829ab6b91e7a3006c3
daliborstakic <stakicdalibor64@gmail.com>
2025-05-11 17:23:22 +0200

    chore(project): add swagger and pino logger

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { User } from '@selune-backend/entities';
 import { UserModule } from '@selune-backend/user';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       isGlobal: true,
       envFilePath: 'apps/api/.env',
     }),
+    LoggerModule.forRoot(),
     CqrsModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
